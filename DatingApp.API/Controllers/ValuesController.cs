040d5154da57a26d8c3c569346be3b58b0fe7e43
RosaRend <rendelrosy@gmail.com>
2018-09-29 21:16:20 -0400

    Able to get a specific value from db

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -20,16 +20,20 @@ namespace DatingApp.API.Controllers
     }
     // GET api/values
     [HttpGet]
-    public IActionResult GetValue()
+    public IActionResult GetValues()
     {
-        var values = _context.Values.ToList():
+        var values = _context.Values.ToList();
+
+        return Ok(values);
     }
 
     // GET api/values/5
     [HttpGet("{id}")]
-    public ActionResult<string> Get(int id)
+    public IActionResult GetValue(int id)
     {
-      return "value";
+        var value = _context.Values.FirstOrDefault(x => x.Id == id);
+
+      return Ok(value);
     }
 
     // POST api/values
