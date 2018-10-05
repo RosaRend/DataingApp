9fdf57d13fefd1f3730379aea832f721b999de94
RosaRend <rendelrosy@gmail.com>
2018-10-04 23:24:32 -0400

    new compnents

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -14,7 +14,12 @@ export class ValueComponent implements OnInit {
   ngOnInit() {
   }
 
-  getValues(){
-    
+  getValues() {
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+
+      this.values = response;
+    }, error => {
+      console.log(error);
+    });
   }
 }
