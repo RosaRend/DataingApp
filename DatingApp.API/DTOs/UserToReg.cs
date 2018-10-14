5825066af6a25ab9b81a955531f07e8a9fc73711
RosaRend <rendelrosy@gmail.com>
2018-10-14 13:03:32 -0400

    User is now required to enter a name and password

--- DatingApp.API/DTOs/UserToReg.cs
+++ DatingApp.API/DTOs/UserToReg.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.DTOs
 {
     public class UserToReg
     {
+        [Required]
+        //^ 'cmd + .' system annottions to get it to work
         public string Username {get; set;}
+        //need to check if the user is actually 
+        //passing in a username and passord
+        // ALL Areas blank registers that user
+        [Required]
+        [StringLength(16, MinimumLength = 8, ErrorMessage = "Password must be more than 8 characters long.")]
         public string Password {get; set;}
     }
+    //Required fixes the problem
 }
